6b53ef468152c5015b95e73cd75b418a2359bfea
unknown <zaid.ahmed@centre.edu>
2019-11-06 14:15:47 -0500

    first office hours

--- graph.js
+++ graph.js
@@ -2,29 +2,32 @@ var penguinPromise=d3.json("classData.json");
 penguinPromise. then(
     function(classroom) 
 {    
-console.log("works",classroom);                  
-drawGraph(classroom)
-    setup(classroom)   
+console.log("works",classroom);                 /* setup(classroom);
+drawGraph(classroom);
+    quizgrade(classroom);*/
+    console.log(makear(classroom));
 })
 var xs = d3.range(400)
 var screen = {width: 600,
              height:500}
-var setup = function(classroom)
-{
+var setup = function(quizes)
+    {
     d3.select("svg")
     .attr("width",screen.width)
     .attr("height",screen.height)
+
+    var xscale = d3.scaleLinear()
+    xscale.domain([0,d3.max(quizes.grade,function(d){
+    return d.grade
+    })])
+    xscale.range([0,screen.width]);
+    var yscale= d3.scaleLinear()
+    yscale.domain(
+    d3.min(quizes, function(d){return    d.grade}),d3.max(quizes,function(d){return d.quizes.grade}))
+    yscale.range([screen.height,0]);
 }
-var xscale = d3.scaleLinear()
-xscale.domain([0,d3.max(quizes.grade,function(classroom){
-    return classroom.grade
-})])
-xscale.range([0,screen.width])
-console.log(xscale(40));
-var yscale= d3.scaleLinear()
-yscale.domain(
-d3.min(classroom, function(p){return classroom.grade}),d3.max(classroom,function(classroom){return classroom.quizes.grade}))
-yscale.range([screen.height,0]);
+
+
 var drawGraph = function(classroom)
 {
     d3.select("svg").selectAll("circle")
@@ -35,3 +38,14 @@ var drawGraph = function(classroom)
     .attr("cy",function(classroom){return classroom.quizes.grade})
     .attr("r",2)
 }
+var getquiz = function(penguin)
+    {
+        console.log(penguin)
+    return penguin.quizes[0].grade;
+    }
+
+
+var makear = function(classroom)
+        {
+     return classroom.map(getquiz)
+        }
