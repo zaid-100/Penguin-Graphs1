67acd5758d93294d88f8732dc0e5c946955f8336
unknown <zaid.ahmed@centre.edu>
2019-11-06 20:49:22 -0500

    second office hours

--- graph.js
+++ graph.js
@@ -1,13 +1,14 @@
 var penguinPromise=d3.json("classData.json");
 penguinPromise. then(
     function(classroom) 
-{    
-console.log("works",classroom);                 /* setup(classroom);
-drawGraph(classroom);
+    {    
+    console.log("works",classroom);                     /* setup(classroom);
+    drawGraph(classroom);
     quizgrade(classroom);*/
-    console.log(makear(classroom));
-})
-var xs = d3.range(400)
+    console.log(makear(classroom, 0));
+var getday = makear(classroom, 0);
+    makegraph(classroom)    
+    })
 var screen = {width: 600,
              height:500}
 var setup = function(quizes)
@@ -25,27 +26,37 @@ var setup = function(quizes)
     yscale.domain(
     d3.min(quizes, function(d){return    d.grade}),d3.max(quizes,function(d){return d.quizes.grade}))
     yscale.range([screen.height,0]);
-}
-
-
-var drawGraph = function(classroom)
-{
-    d3.select("svg").selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx",function(classroom) {console.log(p.x); return classroom.quizes.day})
-    .attr("cy",function(classroom){return classroom.quizes.grade})
-    .attr("r",2)
-}
-var getquiz = function(penguin)
-    {
-        console.log(penguin)
-    return penguin.quizes[0].grade;
     }
 
+var getquiz = function(penguin)
+    {
+    return penguin.quizes[0].grade;
+    
+    }
 
-var makear = function(classroom)
+var makear = function(classroom, day)
         {
-     return classroom.map(getquiz)
+        
+    
+        return classroom.map(function(penguin)
+        {
+        return penguin.quizes[day].grade;
+        })
         }
+var makegraph = function(getday)
+    {
+    d3.select("svg").selectAll("circle")
+    .data(getday)
+    .enter()
+    .append("circle")
+    .attr("cx", function(classroom)
+    {
+    return classroom.quizes.day   
+    })
+    .attr("cy",function(classroom)
+    {
+     return classroom.quizes.grade
+    }
+    )
+    .attr("r",2)    
+    }
